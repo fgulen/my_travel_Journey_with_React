c5fa87b0de83e62b5370dd4d4e566dffec0f878a
fatih gulen <fatih.gulen@outlook.com>
2023-03-05 21:41:39 +0100

    second commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,21 @@
-import fuji from '../images/fuji.png'
 import placeIcon from '../images/place.png'
 
-function Body(props){
-    return(
+function Body(props) {
+    return (
         <div className="main">
             <div className="main-pic-body">
                 <div>
                     {/* <img src={fuji} alt="" /> */}
-                    <img src={process.env.PUBLIC_URL + "/images/" + props.data.imageUrl} alt="" className="card-img" />
+                    <img src={process.env.PUBLIC_URL + "/images/" + props.data.imageUrl} alt={props.data.title} className="card-img" />
 
                 </div>
 
                 <div className='text-wrapper'>
                     <div className="land-google">
-                    <img className='main-img' src={placeIcon} /> <p className='main-land'>{props.data.location} </p> <a className='google-link' href={props.data.googleMapsUrl}>View on Google Maps</a>
+                        <img className='main-img' src={placeIcon} alt="place icon"/> <p className='main-land'>{props.data.location} </p> <a className='google-link' href={props.data.googleMapsUrl}>View on Google Maps</a>
                     </div>
                     <h1 className='main-place'> {props.data.title} </h1>
-                    <p className='main-date'>{props.data.startDate}-{props.data.endDate}</p>
+                    <p className='main-date'>{props.data.startDate} - {props.data.endDate}</p>
                     <p className="main-text">{props.data.description}</p>
                 </div>
             </div>
